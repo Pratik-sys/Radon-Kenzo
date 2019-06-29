be8a55c7caea68b80ffcc1a97237fb92890fdf33
pratik2799 <pithore.pratik@gmail.com>
2019-06-29 20:19:08 +0530

    Revert "sched: Introduce Shadow scheduling"
    This reverts commit c57b52ea410ec95eea4839edab3c6fc8ea869227.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -23,7 +23,6 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
-#include <linux/sched.h>
 #ifdef CONFIG_STATE_NOTIFIER
 #include <linux/state_notifier.h>
 #endif
@@ -190,8 +189,6 @@ static void do_input_boost_rem(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
-	sched_set_shadow_active(false);
-
 	if (sched_boost_active) {
 		ret = sched_set_boost(0);
 		if (ret)
@@ -223,8 +220,6 @@ static void do_input_boost(struct work_struct *work)
 
 	/* Update policies for all online CPUs */
 	update_policy_online();
-
-	sched_set_shadow_active(true);
 
 	/* Enable scheduler boost to migrate tasks to big cluster */
 	if (sched_boost_on_input) {
